0ef4e7f965fda48de5938064c3e736a5e859eba6
João Mateus G Coelho <mateusgamescoelho547@gmail.com>
2021-07-23 14:29:28 -0300

    Edit: Estilizando botão

--- lib/resposta.dart
+++ lib/resposta.dart
@@ -2,15 +2,20 @@ import 'package:flutter/material.dart';
 
 class Resposta extends StatelessWidget {
   final String texto;
-  final void Function() funcaoBotao;
+  final void Function() quandoSelecionado;
 
-  Resposta(this.texto, this.funcaoBotao);
+  Resposta(this.texto, this.quandoSelecionado);
 
   @override
   Widget build(BuildContext context) {
-    return RaisedButton(
-      child: Text(texto),
-      onPressed: () => funcaoBotao(),
+    return Container(
+      width: double.infinity,
+      child: RaisedButton(
+        textColor: Colors.white,
+        color: Colors.blue,
+        child: Text(texto),
+        onPressed: quandoSelecionado,
+      ),
     );
   }
 }
