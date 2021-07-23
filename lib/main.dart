38b0628580a7276fdc67e3cb41b98e94c5e6f106
João Mateus G Coelho <mateusgamescoelho547@gmail.com>
2021-07-23 13:44:27 -0300

    Add: Adicionando botão e testando funcões

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,32 @@ main() {
 }
 
 class PerguntaApp extends StatelessWidget {
+  void responder() {
+    print("Pergunta Respondida!");
+  }
 
   @override
   Widget build(BuildContext context) {
+    final List<String> perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é seu animal favorito?',
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("Perguntas"),
         ),
-        body: Text("Ola mundo!"),
+        body: Column(
+          children: <Widget>[
+            Text(perguntas[0]),
+            // ignore: deprecated_member_use
+            RaisedButton(
+              child: Text("Resposta 1"),
+              onPressed: responder,
+            )
+          ],
+        ),
       ),
     );
   }
