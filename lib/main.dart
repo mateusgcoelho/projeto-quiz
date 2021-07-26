face233fb7424affc7db826491083a9b8d7442d6
João Mateus G Coelho <mateusgamescoelho547@gmail.com>
2021-07-26 13:27:21 -0300

    Edit: Utilizando lista de widgets com map!

--- lib/main.dart
+++ lib/main.dart
@@ -34,6 +34,8 @@ class _PerguntaAppState extends State<PerguntaApp> {
       },
     ];
 
+    List<String> respostas = perguntas[_perguntaSelecionada]['respostas'] as List<String>;
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -42,9 +44,7 @@ class _PerguntaAppState extends State<PerguntaApp> {
         body: Column(
           children: <Widget>[
             Questao(perguntas[_perguntaSelecionada]['texto'].toString()),
-            Resposta('Resposta 1', _responder),
-            Resposta('Resposta 2', _responder),
-            Resposta('Resposta 3', _responder),
+            ...respostas.map((t) => Resposta(t, _responder)).toList(),
           ],
         ),
       ),
