26e8be2de96525b2ff2c11db8c6b1ec35f36d5c0
João Mateus G Coelho <mateusgamescoelho547@gmail.com>
2021-07-26 13:48:32 -0300

    Add: Criando componente de questionario!

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'resposta.dart';
-import './questao.dart';
+import 'package:projeto_perguntas/questionario.dart';
 import './resultado.dart';
 
 main() {
@@ -40,23 +39,17 @@ class _PerguntaAppState extends State<PerguntaApp> {
 
   @override
   Widget build(BuildContext context) {
-    List<String>? respostas = temPerguntaSelecionada
-        ? _perguntas[_perguntaSelecionada]['respostas'] as List<String>
-        : null;
-
     return MaterialApp(
       home: Scaffold(
           appBar: AppBar(
             title: Text("Perguntas"),
           ),
           body: temPerguntaSelecionada
-              ? Column(
-                  children: <Widget>[
-                    Questao(
-                        _perguntas[_perguntaSelecionada]['texto'].toString()),
-                    ...respostas!.map((t) => Resposta(t, _responder)).toList(),
-                  ],
-                )
+              ? Questionario(
+                perguntas: _perguntas,
+                perguntaSelecionada: _perguntaSelecionada,
+                responder: _responder
+              )
               : Resultado()
             ),
     );
