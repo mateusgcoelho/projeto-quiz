d064e7f4235dc05ee9a66502171f50ad289d6a64
João Mateus G Coelho <mateusgamescoelho547@gmail.com>
2021-07-26 13:34:07 -0300

    Add: Criando pagina de parabens!

--- lib/main.dart
+++ lib/main.dart
@@ -53,7 +53,12 @@ class _PerguntaAppState extends State<PerguntaApp> {
             Questao(_perguntas[_perguntaSelecionada]['texto'].toString()),
             ...respostas!.map((t) => Resposta(t, _responder)).toList(),
           ],
-        ) : null
+        ) : Center(
+          child: Text(
+            'Parabéns!',
+            style: TextStyle(fontSize: 28),
+          )
+        )
       ),
     );
   }
